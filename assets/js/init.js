fd9d6ccf5e255156cd7eab8eaaf87e9f58cec521
Perassi Lorenzo <perassi.lorenzo1804@gmail.com>
2025-11-28 13:10:08 +0100

    sistemazione struttura [da sistemare]

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,23 +1,19 @@
-// ELEMENTI
 const popup = document.getElementById("popup");
 const overlay = document.getElementById("popupOverlay");
 
-// APRI POPUP
 function openPopup() {
   popup.classList.add("show");
   overlay.classList.add("show");
 }
 
-// CHIUDI POPUP
 function closePopup() {
   popup.classList.remove("show");
   overlay.classList.remove("show");
 }
 
-// CHIUDI CLICCANDO FUORI
+// Chiudi cliccando fuori
 overlay.addEventListener("click", closePopup);
 
-// ACQUISTA
 function buy() {
   if (confirm("Sei sicuro di voler acquistare?")) {
     window.location.href = "https://www.vinted.it";
